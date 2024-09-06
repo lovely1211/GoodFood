056f0c686fc5ebb4d7c6d6c78381667f13f5cfd0
Lovely Singh <151820018+lovely1211@users.noreply.github.com>
2024-09-06 13:09:56 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('Your_mongodb_connection_string');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
     } catch (err) {
         console.error('MongoDB connection error:', err);
