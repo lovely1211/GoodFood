f3b966aa802c7d2e9d787f8f4e08a62a00b16f48
Lovely Singh <lovely1211zmn@gmail.com>
2024-09-07 14:11:49 +0530

    .

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -6,7 +6,8 @@ const API_URL = 'https://your-backend-url.onrender.com/api';
 // const API_URL = 'http://localhost:5000/api';
 
 const axiosInstance = axios.create({
-  baseURL: API_URL
+  baseURL: API_URL,
+  withCredentials: true
 });
 
 export default axiosInstance;
