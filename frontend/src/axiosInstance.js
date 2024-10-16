25b5d038ff08dd35ead31b89ee4908a86e723682
Lovely Singh <lovely1211zmn@gmail.com>
2024-10-16 23:06:38 +0530

    .

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-// Define your base URL here once
-// const API_URL = 'https://your-backend-url.onrender.com/api'; 
-
 const API_URL = 'http://localhost:5000/api';
 
 const axiosInstance = axios.create({
