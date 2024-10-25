227442405e657d42c1371daa511899ee31bdfb55
Lovely Singh <lovely1211zmn@gmail.com>
2024-10-25 22:18:29 +0530

    error

--- backend/services/siteQueryEmail.js
+++ backend/services/siteQueryEmail.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 
-// Email configuration
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -15,20 +15,19 @@ const transporter = nodemailer.createTransport({
 });
 
 router.post('/sendQuery', (req, res) => {
-  const { queryType, name, email, message, isMember } = req.body;
+  const { queryType, name, email, message } = req.body;
 
-  if (!queryType || !name || !email || !message || isMember === undefined) {
+  if (!queryType || !name || !email || message === undefined) {
     return res.status(400).send('All fields are required.');
   }
 
   const mailOptions = {
-    from: email,
-    to: process.env.EMAIL_USER, //  email
+    from: process.env.EMAIL_USER, 
+    to: process.env.EMAIL_USER, 
     subject: `Query from ${name} - ${queryType}`,
     text: `
       Name: ${name}
       Email: ${email}
-      Member of GoodFood: ${isMember}
       Message: ${message}
     `
   };
